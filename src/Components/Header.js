5cef05e6b10d7b4fe787fe32dfe101d772451f31
Dimitar Vassilev <d.v.vassilev@gmail.com>
2020-11-08 20:18:49 +0200

    feat/ Added Header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,10 +1,35 @@
 import React from 'react'
-
+import style from '../styles/header.module.css'
+import Logo from './Logo'
+import {Link} from 'react-router-dom'
 function Header() {
     return (
-        <div>
-            
+    <div className={style.navbar}>
+        <div className={style.dropdown}>   
+            <Logo />
         </div>
+        
+        <div className={`${style.dropdown} ${style.dropdown_margin}`}>
+            <button className={style.dropbtn}>За нас</button>
+            <div className={style.dropdown_content}>
+                <Link to='/management-board'>Управителен съвет</Link>
+                <Link to='/activities'>Дейности</Link>
+                <Link to='/reports'>Отчети</Link>
+            </div>    
+        </div>
+        <a href="novini.html" className={style.marginFromTop}>Новини</a>
+        <div className={style.dropdown}>
+            <button className={style.dropbtn}>Проекти</button>
+            <div className={style.dropdown_content}>
+                <Link to='/current-projects'>Текущи</Link>
+                <Link to='/finished-projects'>Приключили</Link>
+                <Link to='/archive'>Архив</Link>
+            </div>  
+        </div> 
+        <Link to='/gallery' className={style.marginFromTop}>Галерия</Link>
+        <Link to='/Privacy-and-data-protection-policy' className={style.marginFromTop}>Политика за поверителност и защита на личните данни</Link>
+        <Link to='/contacts' className={style.marginFromTop}>Контакти</Link>
+    </div>
     )
 }
 
--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import Header from '../Components/Header'
 
 function HomePage() {
     return (
         <div>
+            <Header />
             <p>this is home</p>
         </div>
     )
