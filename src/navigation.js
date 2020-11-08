b72134869a35c32623386386c7f85bb4463ad806
Dimitar Vassilev <d.v.vassilev@gmail.com>
2020-11-08 15:54:24 +0200

    feat/ Added navigation, home and about us page and header component

--- src/navigation.js
+++ src/navigation.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
+import HomePage from "./Pages/HomePage"
+import AboutUs from "./Pages/AboutUs"
 
 const Navigation = () => {
     return (
         <div>
-            
+            <BrowserRouter>
+                <Switch>
+                    <Route path="/" exact component={HomePage}/>
+                    <Route path="/aboutus" exact component={AboutUs}/>
+                </Switch>
+            </BrowserRouter>
         </div>
     )
 }
